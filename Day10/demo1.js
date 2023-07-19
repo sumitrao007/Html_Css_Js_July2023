461c945bdf864e2233854b0ae87a59c4d097309c
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-07-19 19:42:33 +0530

    Day10

---
+++ Day10/demo1.js
@@ -0,0 +1,4 @@
+
+console.log("Hello Js ----");
+
+document.write("Hello Js");
---
+++ Day10/demo2.js
@@ -0,0 +1,17 @@
+// console.log("Hopes So U r Enjoying JS by Sumit Baba");
+
+var a1,a2;
+
+a1=10;
+
+// console.log("Value of a1 is "+a1);
+// console.log('Value of a1 is '+a1);
+a1='Sumit';
+// console.log(a1);
+
+// console.log(` ------ value of a1 ----  ${a1}`);
+
+
+
+
+
