2327bc0e4b065e61f0828c9e3a2fb9834edcc9d0
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2023-07-20 19:37:54 +0530

    Day11

---
+++ Day11/demo1.js
@@ -0,0 +1,79 @@
+
+//operators
+// Airthmathic => +,-,*,/,%
+// Logical => &&, ||,!
+// Unray => post/pre inc/dec ++a,a--
+//ternary => condition?expression1:expression2
+// bitwise => &,|,^,!,~,<<,>>,
+// relation => <,>,<=,>=,!=,==(it checks value only ), === (strongly equality => it checks value as well as its data type )
+//assignment => =,+=,-=,*=,/=,%=
+
+
+// Control Statement/Sequential sequance
+// if,if-else,nested if-else,switch,break,continue
+
+var a=50;
+// if(a<20){
+//     console.log("Condition is true.")
+// }else{
+//     console.log("Condition is False.")
+// }
+
+var choice=2;
+// switch(choice){
+//     case 1: console.log("U r in case 1");
+//             break;
+//     case 2: console.log("U r in case 2");
+//             break;
+//     default : console.log("u R in Default case");
+//             break;
+
+// }
+
+//loop statement
+// while ,do-while,for,froeach
+
+var count=4;
+
+// while(count!=0){
+//     console.log("Count is "+count);
+//     count--;
+// }
+
+// do{
+//     console.log("Count is "+count);
+//     count--;
+// }while(count!=0)
+
+
+
+//for loop 
+// for(var i=0;i<4;i++){
+//     console.log("Value of i is "+i);
+// }
+// console.log("Access i after the loop "+i);
+
+// var => scope  => global scope 
+// let keywords 
+// scope => within nearest block ({})
+
+for(let i=0;i<4;i++){
+    console.log("Value of i is "+i);
+}
+
+// console.log("Access i after the loop "+i);
+
+//const => it set ur value as constant
+// scope => global as well as local scope 
+
+const pi=3.14;
+
+
+
+
+
+
+
+
+
+
